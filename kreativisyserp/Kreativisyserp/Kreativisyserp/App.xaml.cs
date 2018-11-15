598137875e68a97d6685b8a8aeecfced597f8356
Juan Francisco Estrada Garfias <francisco.estrada@kreativisys.com>
2018-11-14 22:20:44 -0600

    Initial project

---
+++ kreativisyserp/Kreativisyserp/Kreativisyserp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Kreativisyserp
+{
+    using Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+	{
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            //pagina inicial de la aplicacion
+            MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
